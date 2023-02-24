0f4ecf7b1a6f4d8d5b89966e43d502f70824f7f4
Munyaradzi <syntaxread3r@gmail.com>
2023-02-24 22:03:42 +0000

    create index.js

--- login.php
+++ login.php
@@ -14,5 +14,6 @@
     </form>
 
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="./index.js"></script>
 </body>
 </html>
