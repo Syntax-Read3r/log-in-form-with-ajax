5d60838edfd68aef75b024df179e22932aa92727
Munyaradzi <syntaxread3r@gmail.com>
2023-02-25 02:45:09 +0000

    finish the project, with no styles

--- hidden.php
+++ hidden.php
@@ -1,11 +1,13 @@
 <?php
     session_start();
 
-    if(!isset($_SESSION['logenIN'])) {
+    if(!isset($_SESSION['loggedIN'])) {
         header(header: 'Location: login.php');
         exit();
     }
 
 ?>
+
+ <a href="logout.php">Log Out</a><br>
 
 You are logged in successfully
